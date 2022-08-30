04af9b6501459b65e0cfb2e6837155e29861c867
Samandar000 <samandarnfh.2020@gmail.com>
2022-08-30 12:31:43 +0500

    10

--- List10.py
+++ List10.py
@@ -6,6 +6,48 @@ def main(list1):
     Returns:
         list: return answer
     """
-    list1 =  list1.count(1) , list1.count(0)
+    zeros = list1.count(0) 
+    ones = list1.count(1)
+    list0= [ones,zeros]
+    
+    
+    return list0
 
-    return list1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     x=0
+#     y=0
+#     i=0
+#     while i < len(list1):
+#         if list1[i] == (0):
+#             list1 == list1.append(1)
+#             y+=1
+#         i+=1
+#     while i < len(list1):
+#         if list1[i] == (0):
+#             list1 == list1.append(1)
+#             x+=1
+#         i+=1
+#     return x
+# print(main([0, 0, 0, 0, 1, 0, 1, 0]))
